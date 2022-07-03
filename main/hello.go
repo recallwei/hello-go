9ffa1fa9418644884b770d3a20271b2fc93ae182
Bruce Song <recall4056@gmail.com>
2022-07-03 22:36:58 +0800

    Remove files.

--- main/hello.go
+++ main/hello.go
@@ -1,18 +1,5 @@
 package main
 
-import (
-	"example/greetings"
-	"fmt"
-	"log"
-
-	"rsc.io/quote"
-)
-
 func main() {
-	fmt.Println(quote.Go())
-	message, err := greetings.Hello("Bruce")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(message)
+
 }
